cfd9f2be039cbff38018c73cb5107d52ba69d761
민재로 <111555810+Minjaero@users.noreply.github.com>
2023-03-29 11:06:11 +0900

    Add files via upload

---
+++ K_0329.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class K_0329 {
+	public static void main(String[] args) {
+
+//		int sum = 0;
+//		System.out.println("1-2+3-4+5-6+7-8+9-10=");
+//		if (i % 2 ==0)
+//		else 
+//			System.out.println(sum);
+		
+//		for(int i=1; i<6; i++)
+//			for(int j=1; j<6; j++)
+//				System.out.println(j +"\t");
+//			System.out.println("\n");
+		
+//		for (int i = 1; i<=5; i++)
+//			for(int j =1; j<=5; j++)
+//				System.out.println(j + "\t");
+//			System.out.println("\n");
+		
+		for( int i =1; i<3; i++)
+			for(int j = 3; i<3; j++)
+				System.out.print(j + "\t");
+			System.out.print("*\n");
+		
+	}
+		
+}
