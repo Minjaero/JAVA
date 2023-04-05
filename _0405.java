3967037bd808776c536b548613f97e7a707723b1
민재로 <111555810+Minjaero@users.noreply.github.com>
2023-04-05 09:30:00 +0900

    Add files via upload

---
+++ _0405.java
@@ -0,0 +1,49 @@
+package _0405;
+
+public class _0405 {
+	public static void main(String[] args) {
+		
+//		int i=1;
+//		while(i <= 5) {
+//			System.out.println(i + "\t");
+//			i++;
+//		}
+		
+//		int i =1 , sum = 0;
+//		System.out.println("1-2+3-4+5-6+7-8+9-10=");
+//		while(i<=10) {
+//			if(i % 2==0) sum -=i;
+//			else sum += i;
+//			i++;
+//		}
+//		System.out.println(sum);
+		
+//		int i = 5;
+//		do {
+//			System.out.println(i);
+//			i++;
+//		}	while( i > 10);
+		
+//		for(int i=1; i<=10; i++) {
+//			if(i==5) break;
+//			System.out.print(i + "\t");
+//		}
+//		for(int i = 1; i<=10; i++) {
+//			System.out.print(i + "\t");
+//			if( i=5) break;
+//		}
+		
+		for (int i =1; i<=10; i++) {
+			if (i==5) continue;
+			System.out.print(i + "\t");
+		}
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
